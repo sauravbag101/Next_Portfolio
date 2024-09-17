fc62058b15b6b4b0d4ca083441a0d9423c5a58d0
sauravbag101 <sauravbag101@gmail.com>
2024-09-17 16:24:25 +0530

    for

--- my-app/components/main/sub/SkillDataProvider.tsx
+++ my-app/components/main/sub/SkillDataProvider.tsx
@@ -1,42 +1,46 @@
 "use client"
 import React from 'react'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import Image from 'next/image'
 
-interface Props{
+interface Props {
     src: string;
     width: number;
     height: number;
     index: number;
 }
 
-const skillDataProvider = ({src, width,height,index} : Props) => {
-    const{ref,inView} = useInView({
-    triggerOnce:true
-    })
-    const imageVariants = {
-        hidden:{opacity: 0},
-        visible: {opacity: 1}
-    }
-    const animationDelay = 0.3
-  return (
-    <motion.div 
-    ref={ref}
-    initial="hidden"
-    variants={imageVariants}
-    animate={inView ? "visible" : "hidden"}
-    custom={index}
-    transition={{delay: index * animationDelay}}>
+// Rename the component to start with an uppercase letter
+const SkillDataProvider = ({ src, width, height, index }: Props) => {
+    const { ref, inView } = useInView({
+        triggerOnce: true
+    });
 
-    <Image 
-    src={src} 
-    width={width} 
-    height={height} 
-    alt="skill img"/>    
-      
-    </motion.div>
-  )
+    const imageVariants = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 }
+    };
+
+    const animationDelay = 0.3;
+
+    return (
+        <motion.div 
+            ref={ref}
+            initial="hidden"
+            variants={imageVariants}
+            animate={inView ? "visible" : "hidden"}
+            custom={index}
+            transition={{ delay: index * animationDelay }}
+        >
+            <Image 
+                src={src} 
+                width={width} 
+                height={height} 
+                alt="skill img" 
+            />    
+        </motion.div>
+    );
 }
 
-export default skillDataProvider
+export default SkillDataProvider;
